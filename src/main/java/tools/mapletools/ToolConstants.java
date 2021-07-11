e2f1981dc4eb047db824a3c38334a5caffa6ca1a
P0nk <ponkcode@gmail.com>
2021-07-11 10:54:29 +0200

    Move MapleIdRetriever to main module

--- src/main/java/tools/mapletools/ToolConstants.java
+++ src/main/java/tools/mapletools/ToolConstants.java
@@ -2,17 +2,17 @@ package tools.mapletools;
 
 import java.io.File;
 
-public class ToolConstants {
-    public static final File INPUT_DIRECTORY = new File("tools/input");
-    public static final File OUTPUT_DIRECTORY = new File("tools/output");
-    public static final String SCRIPTS_PATH = "scripts";
-    public static final String HANDBOOK_PATH = "handbook";
+class ToolConstants {
+    static final File INPUT_DIRECTORY = new File("tools/input");
+    static final File OUTPUT_DIRECTORY = new File("tools/output");
+    static final String SCRIPTS_PATH = "scripts";
+    static final String HANDBOOK_PATH = "handbook";
 
-    public static File getInputFile(String fileName) {
+    static File getInputFile(String fileName) {
         return new File(INPUT_DIRECTORY, fileName);
     }
 
-    public static File getOutputFile(String fileName) {
+    static File getOutputFile(String fileName) {
         return new File(OUTPUT_DIRECTORY, fileName);
     }
 }
