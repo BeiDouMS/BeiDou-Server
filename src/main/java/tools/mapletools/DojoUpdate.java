d09b2371573af894647b3c8e54baf174f981a8c6
P0nk <ponkcode@gmail.com>
2021-07-11 13:52:33 +0200

    Move MapleDojoUpdate to main module, use existing wz files

--- src/main/java/tools/mapletools/DojoUpdate.java
+++ src/main/java/tools/mapletools/DojoUpdate.java
@@ -1,70 +1,42 @@
-/*
-    This file is part of the HeavenMS MapleStory Server
-    Copyleft (L) 2016 - 2019 RonanLana
+package tools.mapletools;
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation version 3 as published by
-    the Free Software Foundation. You may not use, modify or distribute
-    this program under any other version of the GNU Affero General Public
-    License.
+import provider.wz.WZFiles;
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-package mapledojoupdate;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.sql.Connection;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
- *
  * @author RonanLana
- 
- This application parses the Character.wz folder inputted and adds/updates the "info/level"
- node on every known equipment id. This addition enables client-side view of the equipment
- level attribute on every equipment in the game, given proper item visibility, be it from
- own equipments or from other players.
- 
- Estimated parse time: 10 seconds
- 
+ * <p>
+ * This application parses the Character.wz folder inputted and adds/updates the "info/level"
+ * node on every known equipment id. This addition enables client-side view of the equipment
+ * level attribute on every equipment in the game, given proper item visibility, be it from
+ * own equipments or from other players.
+ * <p>
+ * Estimated parse time: 10 seconds
  */
-public class MapleDojoUpdate {
+public class DojoUpdate {
+    private static final File INPUT_DIRECTORY = new File(WZFiles.MAP.getFile(), "/Map/Map9");
+    private static final File OUTPUT_DIRECTORY = ToolConstants.getOutputFile("dojo-maps");
+    private static final int DOJO_MIN_MAP_ID = 925_020_100;
+    private static final int DOJO_MAX_MAP_ID = 925_033_804;
+    private static final int INITIAL_STRING_LENGTH = 250;
 
-    static String dojoDirectory = "lib/original/";
-    static String outputDirectory = "lib/updated/";
-    
-    static Connection con = null;
-    static PrintWriter printWriter = null;
-    static InputStreamReader fileReader = null;
-    static BufferedReader bufferedReader = null;
-    
-    static int initialStringLength = 250;
-    static boolean isDojoMapid;
-    
-    static byte status;
-    
+    private static PrintWriter printWriter = null;
+    private static BufferedReader bufferedReader = null;
+    private static boolean isDojoMapid;
+    private static byte status;
+
     private static String getName(String token) {
         int i, j;
         char[] dest;
         String d;
-        
+
         i = token.lastIndexOf("name");
         i = token.indexOf("\"", i) + 1; //lower bound of the string
         j = token.indexOf("\"", i);     //upper bound
 
-        dest = new char[initialStringLength];
+        dest = new char[INITIAL_STRING_LENGTH];
         try {
             token.getChars(i, j, dest, 0);
         } catch (StringIndexOutOfBoundsException e) {
@@ -75,59 +47,56 @@ public class MapleDojoUpdate {
             e.printStackTrace();
             try {
                 Thread.sleep(100000000);
-            } catch (Exception ex) {}
+            } catch (Exception ex) {
+            }
         }
-        
+
 
         d = new String(dest);
-        return(d.trim());
+        return (d.trim());
     }
-    
+
     private static void forwardCursor(int st) {
         String line = null;
 
         try {
-            while(status >= st && (line = bufferedReader.readLine()) != null) {
+            while (status >= st && (line = bufferedReader.readLine()) != null) {
                 simpleToken(line);
                 printWriter.println(line);
             }
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     private static void simpleToken(String token) {
-        if(token.contains("/imgdir")) {
+        if (token.contains("/imgdir")) {
             status -= 1;
-        }
-        else if(token.contains("imgdir")) {
+        } else if (token.contains("imgdir")) {
             status += 1;
         }
     }
-    
+
     private static void translateToken(String token) {
-        if(token.contains("/imgdir")) {
+        if (token.contains("/imgdir")) {
             status -= 1;
             printWriter.println(token);
-        }
-        else if(token.contains("imgdir")) {
+        } else if (token.contains("imgdir")) {
             printWriter.println(token);
             status += 1;
-            
+
             if (status == 2) {
                 String d = getName(token);
-                if(!d.contentEquals("info")) {
+                if (!d.contentEquals("info")) {
                     forwardCursor(status);
                 }
             } else if (status > 2) {
                 forwardCursor(status);
             }
-        }
-        else {
+        } else {
             if (status == 2 && isDojoMapid) {
                 String item = getName(token);
-                
+
                 if (item.contentEquals("onFirstUserEnter")) {
                     printWriter.println("    <string name=\"onFirstUserEnter\" value=\"dojang_1st\"/>");
                 } else if (item.contentEquals("onUserEnter")) {
@@ -140,24 +109,27 @@ public class MapleDojoUpdate {
             }
         }
     }
-    
+
     private static int getMapId(String fileName) {
         return Integer.parseInt(fileName.substring(0, 9));
     }
-    
+
     private static void parseDojoData(File file, String curPath) throws IOException {
-        printWriter = new PrintWriter(outputDirectory + curPath + file.getName(), "UTF-8");
-        
-        fileReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+        int mapId = getMapId(file.getName());
+        isDojoMapid = isDojoMapId(mapId);
+        if (!isDojoMapid) {
+            return;
+        }
+
+        printWriter = new PrintWriter(OUTPUT_DIRECTORY.getPath() + "/" + curPath + file.getName(), StandardCharsets.UTF_8);
+
+        InputStreamReader fileReader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
         bufferedReader = new BufferedReader(fileReader);
-        
+
         status = 0;
-        
-        int mapid = getMapId(file.getName());
-        isDojoMapid = mapid >= 925020100 && mapid < 925040000;
-        
+
         String line;
-        while((line = bufferedReader.readLine()) != null) {
+        while ((line = bufferedReader.readLine()) != null) {
             translateToken(line);
         }
 
@@ -167,35 +139,35 @@ public class MapleDojoUpdate {
         printFileFooter();
         printWriter.close();
     }
-    
+
+    private static boolean isDojoMapId(int mapId) {
+        return mapId >= DOJO_MIN_MAP_ID && mapId <= DOJO_MAX_MAP_ID;
+    }
+
     private static void printFileFooter() {
         printWriter.println("<!--");
         printWriter.println(" # WZ XML File updated by the MapleDojoUpdate feature by Ronan Lana.");
         printWriter.println(" # Generated data takes into account info from the server-side WZ.xmls.");
         printWriter.println("-->");
     }
-    
+
     private static void parseDirectoryDojoData(String curPath) {
-        File folder = new File(outputDirectory + curPath);
+        File folder = new File(OUTPUT_DIRECTORY, curPath);
         if (!folder.exists()) {
             folder.mkdir();
         }
-        
+
         System.out.println("Parsing directory '" + curPath + "'");
-        folder = new File(dojoDirectory + curPath);
+        folder = new File(INPUT_DIRECTORY, curPath);
         for (File file : folder.listFiles()) {
             if (file.isFile()) {
                 try {
                     parseDojoData(file, curPath);
-                }
-                catch(FileNotFoundException ex) {
+                } catch (FileNotFoundException ex) {
                     System.out.println("Unable to open dojo file " + file.getAbsolutePath() + ".");
-                }
-                catch(IOException ex) {
+                } catch (IOException ex) {
                     System.out.println("Error reading dojo file " + file.getAbsolutePath() + ".");
-                }
-
-                catch(Exception e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             } else {
@@ -203,9 +175,8 @@ public class MapleDojoUpdate {
             }
         }
     }
-    
+
     public static void main(String[] args) {
         parseDirectoryDojoData("");
     }
-    
 }
