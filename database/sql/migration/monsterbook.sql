ecca4465057633e5646cb36f48dd4e734c902342
P0nk <ponkcode@gmail.com>
2022-01-19 18:04:36 +0100

    Add helpful comment in the monsterbook script

--- database/sql/migration/monsterbook.sql
+++ database/sql/migration/monsterbook.sql
@@ -1,3 +1,5 @@
+/*  Manually run this script in MySQL Workbench or some other database client
+    to migrate your old (pre Jan 19th 2022) monsterbook table to the new version */
 ALTER TABLE cosmic.`monsterbook`
 CHANGE COLUMN `charid` `charid` INT(11) NOT NULL,
 ADD PRIMARY KEY (`charid`, `cardid`),
