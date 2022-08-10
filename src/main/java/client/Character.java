85e5356a9c8adc6970bd89818c034ac0452fab5f
Đạt Nhân Trương <truongdatnhan@gmail.com>
2022-08-11 01:33:48 +0700

    Moving checking storage null to when loading char

--- src/main/java/client/Character.java
+++ src/main/java/client/Character.java
@@ -7402,10 +7402,19 @@ public class Character extends AbstractCharacterObject {
                         }
                     }
                 }
-
+                
                 ret.buddylist.loadFromDb(charid);
                 ret.storage = wserv.getAccountStorage(ret.accountid);
-
+                /*
+                 * Bugs when player first time into server
+                 * The storage won't exist so nothing to load
+                 * and must wait until next login
+                 */
+                if(ret.storage == null) {
+                    wserv.loadAccountStorage(ret.accountid);
+                    ret.storage = wserv.getAccountStorage(ret.accountid);
+                }
+                
                 int startHp = ret.hp, startMp = ret.mp;
                 ret.reapplyLocalStats();
                 ret.changeHpMp(startHp, startMp, true);
@@ -8295,15 +8304,6 @@ public class Character extends AbstractCharacterObject {
                         }
                         ps.executeBatch();
                     }
-                }
-
-                /*
-                 * Bugs when player first time into server
-                 * The storage won't get initialize
-                 * and must wait until next login
-                 */
-                if (getStorage() == null) {
-                    Server.getInstance().getWorld(world).loadAccountStorage(accountid);
                 }
 
                 con.commit();
