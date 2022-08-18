c96e4a0e67991df5332f44a9048b8cd7cd41d9cc
Charlie Unfricht <dzcharlie@gmail.com>
2022-08-17 20:36:28 -0400

    Add missing skill IDs for warrior tree

--- src/main/java/constants/skills/DragonKnight.java
+++ src/main/java/constants/skills/DragonKnight.java
@@ -28,6 +28,8 @@ public class DragonKnight {
     public static final int ELEMENTAL_RESISTANCE = 1310000;
     public static final int SPEAR_CRUSHER = 1311001;
     public static final int POLE_ARM_CRUSHER = 1311002;
+    public static final int SPEAR_DRAGON_FURY = 1311003;
+    public static final int POLE_ARM_DRAGON_FURY = 1311004;
     public static final int SACRIFICE = 1311005;
     public static final int DRAGON_ROAR = 1311006;
     public static final int POWER_CRASH = 1311007;
