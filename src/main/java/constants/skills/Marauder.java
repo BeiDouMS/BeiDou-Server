435ca37a71d3a84ee59e521d70e09025058ff142
Charlie Unfricht <dzcharlie@gmail.com>
2022-08-17 20:26:14 -0400

    Add missing skill IDs for pirate tree

--- src/main/java/constants/skills/Marauder.java
+++ src/main/java/constants/skills/Marauder.java
@@ -27,6 +27,8 @@ package constants.skills;
 public class Marauder {
     public static final int STUN_MASTERY = 5110000;
     public static final int ENERGY_CHARGE = 5110001;
+    public static final int ENERGY_BLAST = 5111002;
     public static final int ENERGY_DRAIN = 5111004;
     public static final int TRANSFORMATION = 5111005;
+    public static final int SHOCKWAVE = 5111006;
 }
--- src/main/java/constants/skills/Pirate.java
+++ src/main/java/constants/skills/Pirate.java
@@ -25,5 +25,9 @@ package constants.skills;
  * @author BubblesDev
  */
 public class Pirate {
+    public static final int BULLET_TIME = 5000000;
+    public static final int FLASH_FIST = 5001001;
+    public static final int SOMERSAULT_KICK = 5001002;
+    public static final int DOUBLE_SHOT = 5001003;
     public static final int DASH = 5001005;
 }
