991873dcbea71890e40d2bcd4b06186663a227c4
Charlie Unfricht <dzcharlie@gmail.com>
2022-09-23 17:32:34 -0400

    Update ritual stone reactor script (see PR 135 discussion)

--- scripts/reactor/1029000.js
+++ scripts/reactor/1029000.js
@@ -1,5 +1,7 @@
 function act() {
-    rm.killMonster(3230300);
-    rm.killMonster(3230301);
-    rm.message("The Jr. Boogies have been scared away.");
+    if (rm.isAllReactorState(1029000, 0x04)) { // 0x04 appears to be the destroyed state
+        rm.killMonster(3230300);
+        rm.killMonster(3230301);
+        rm.playerMessage(6, "Once the rock crumbled, Jr. Boogie was in great pain and disappeared.");
+    }    
 }
