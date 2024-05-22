01cd0489373a35d3457b93e957dd92e50b934656
昨日小睡 <1099365937@qq.com>
2024-05-23 00:34:06 +0800

    增加banner、log4j2.xml

--- gms-api/src/main/java/org/gms/ApiApplication.java
+++ gms-api/src/main/java/org/gms/ApiApplication.java
@@ -1,12 +1,15 @@
 package org.gms;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("org.gms.mapper")
 public class ApiApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 启动Spring Boot应用
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
