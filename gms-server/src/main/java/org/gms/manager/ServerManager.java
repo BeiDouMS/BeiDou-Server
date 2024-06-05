095b6d914703c9efbf6ef3e06eeec023c0ea5749
昨日小睡 <1099365937@qq.com>
2024-06-05 20:41:47 +0800

    增加提示

--- gms-server/src/main/java/org/gms/manager/ServerManager.java
+++ gms-server/src/main/java/org/gms/manager/ServerManager.java
@@ -2,19 +2,27 @@ package org.gms.manager;
 
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.gms.net.server.Server;
+import org.springdoc.core.properties.SpringDocConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+
 @Component
+@Slf4j
 public class ServerManager implements ApplicationContextAware, ApplicationRunner, DisposableBean {
     @Getter
     private static ApplicationContext applicationContext;
+
     @Override
     public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
         ServerManager.applicationContext = applicationContext;
@@ -23,6 +31,13 @@ public class ServerManager implements ApplicationContextAware, ApplicationRunner
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Server.getInstance().init();
+
+        SpringDocConfigProperties springDocConfigProperties = applicationContext.getBean(SpringDocConfigProperties.class);
+        SwaggerUiConfigProperties swaggerUiConfigProperties = applicationContext.getBean(SwaggerUiConfigProperties.class);
+        if (springDocConfigProperties.getApiDocs().isEnabled() && swaggerUiConfigProperties.isEnabled()) {
+            Environment environment = applicationContext.getBean(Environment.class);
+            log.info("接口文档地址：{}", "http://" + InetAddress.getLocalHost().getHostAddress() + ":" + environment.getProperty("server.port") + "/swagger-ui.html");
+        }
     }
 
     @Override
