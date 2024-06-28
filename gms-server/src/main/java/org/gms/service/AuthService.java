775a84285d8568d54ba4cda0997fdc331fc488ab
leevccc <94272011+leevccc@users.noreply.github.com>
2024-06-28 17:19:08 +0800

    修复登录接口，账号密码错误时的请求结果为success的问题

--- gms-server/src/main/java/org/gms/service/AuthService.java
+++ gms-server/src/main/java/org/gms/service/AuthService.java
@@ -1,7 +1,9 @@
 package org.gms.service;
 
+import org.gms.util.I18nUtil;
 import org.gms.util.JwtUtils;
 import org.gms.dao.entity.AccountsDO;
+import org.gms.util.RequireUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +23,8 @@ public class AuthService {
 
     public Map<String, String> getToken(String name, String password) {
         AccountsDO account = accountService.findByName(name);
-        if (account == null || !accountService.checkPassword(password, account)) return null;
+        RequireUtil.requireFalse(account == null || !accountService.checkPassword(password, account),
+                I18nUtil.getExceptionMessage("AuthService.account.or.password.error"));
 
         HashMap<String, String> result = new HashMap<>();
         result.put("token", jwtUtils.generateJwtToken(account.getName()));
