84969bf73b8d346e334d25521de55ad1a01eb82c
leevccc <94272011+leevccc@users.noreply.github.com>
2024-07-03 00:51:45 +0800

    修复：config.yaml有中文时启动报错的问题

--- src/main/java/config/YamlConfig.java
+++ src/main/java/config/YamlConfig.java
@@ -1,10 +1,10 @@
 package config;
 
 import com.esotericsoftware.yamlbeans.YamlReader;
-import constants.string.CharsetConstants;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -19,7 +19,7 @@ public class YamlConfig {
 
     private static YamlConfig loadConfig() {
         try {
-            YamlReader reader = new YamlReader(Files.newBufferedReader(Path.of(CONFIG_FILE_NAME), CharsetConstants.CHARSET));
+            YamlReader reader = new YamlReader(Files.newBufferedReader(Path.of(CONFIG_FILE_NAME), StandardCharsets.UTF_8));
             YamlConfig config = reader.read(YamlConfig.class);
             reader.close();
             return config;
