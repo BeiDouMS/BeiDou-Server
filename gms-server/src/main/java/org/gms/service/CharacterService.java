3d7664e1a31bfd2f84448679c57c1e4bf45a5b29
昨日小睡 <1099365937@qq.com>
2024-07-10 15:41:19 +0800

    修复查询判断的错误

--- gms-server/src/main/java/org/gms/service/CharacterService.java
+++ gms-server/src/main/java/org/gms/service/CharacterService.java
@@ -13,13 +13,12 @@ import org.gms.net.server.Server;
 import org.gms.net.server.world.World;
 import org.gms.util.BasePageUtil;
 import org.gms.util.I18nUtil;
+import org.gms.util.RequireUtil;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
 import java.util.Collection;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static org.gms.dao.entity.table.ExtendValueDOTableDef.EXTEND_VALUE_D_O;
 
@@ -31,9 +30,9 @@ public class CharacterService {
     public Page<ChrOnlineListRtnDTO> getChrOnlineList(ChrOnlineListReqDTO request) {
         Collection<Character> chrList = Server.getInstance().getWorld(request.getWorld()).getPlayerStorage().getAllCharacters();
         return BasePageUtil.create(chrList, request)
-                .filter(chr -> Objects.equals(chr.getId(), request.getId())
-                        || Objects.equals(chr.getName(), request.getName())
-                        || Objects.equals(chr.getMap().getId(), request.getMap()))
+                .filter(chr -> Objects.isNull(request.getId()) || Objects.equals(chr.getId(), request.getId())
+                        || RequireUtil.isEmpty(request.getName()) || Objects.equals(chr.getName(), request.getName())
+                        || Objects.isNull(request.getMap()) || Objects.equals(chr.getMap().getId(), request.getMap()))
                 .page(chr -> ChrOnlineListRtnDTO.builder()
                         .id(chr.getId())
                         .name(chr.getName())
