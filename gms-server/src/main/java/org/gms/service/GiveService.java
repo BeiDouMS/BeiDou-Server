2f89aef513d6e46ff7612fd8e764f93b3c126267
昨日小睡 <1099365937@qq.com>
2024-07-16 11:19:40 +0800

    1.支持读取外置application.yml 2.接口支持发gm、人气

--- gms-server/src/main/java/org/gms/service/GiveService.java
+++ gms-server/src/main/java/org/gms/service/GiveService.java
@@ -2,9 +2,11 @@ package org.gms.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.gms.client.Character;
+import org.gms.client.Stat;
 import org.gms.client.inventory.Pet;
 import org.gms.client.inventory.manipulator.InventoryManipulator;
 import org.gms.constants.inventory.ItemConstants;
+import org.gms.constants.string.ExtendType;
 import org.gms.dao.entity.ExtendValueDO;
 import org.gms.dto.GiveResourceReqDTO;
 import org.gms.exception.BizException;
@@ -77,7 +79,11 @@ public class GiveService {
         if (wId == null || wId < 0 || cId == null || cId < 1) {
             throw new BizException("大区 ID 或者 玩家 ID 不正确");
         }
-
+        Character chr = Server.getInstance()
+                .getWorlds().get(wId)
+                .getPlayerStorage().getCharacterById(cId);
+        if (chr == null) throw new BizException("玩家已离线");
+        
         switch (submitData.getType()) {
             case 0: // nxCredit 点券
             case 1: // nxPrepaid 信用点券
@@ -87,19 +93,19 @@ public class GiveService {
                     case 2 -> CashShop.MAPLE_POINT;
                     default -> CashShop.NX_CREDIT;
                 };
-                giveNxChr(wId, cId, submitData.getQuantity(), cashType);
+                giveNxChr(chr, submitData.getQuantity(), cashType);
                 break;
             case 3: // mesos
-                giveMesosChr(wId, cId, submitData.getQuantity());
+                giveMesosChr(chr, submitData.getQuantity());
                 break;
             case 4: // exp
-                giveExpChr(wId, cId, submitData.getQuantity());
+                giveExpChr(chr, submitData.getQuantity());
                 break;
             case 5: // item
-                giveItemChr(wId, cId, submitData.getId(), Short.parseShort(submitData.getQuantity().toString()));
+                giveItemChr(chr, submitData.getId(), Short.parseShort(submitData.getQuantity().toString()));
                 break;
             case 6: // equip
-                giveEquipChr(wId, cId, submitData);
+                giveEquipChr(chr, submitData);
                 break;
             case 7: // expRate
             case 8: // mesosRate
@@ -112,7 +118,13 @@ public class GiveService {
                     case 10 -> "bossRate";
                     default -> "None";
                 };
-                giveRateChr(wId, cId, rateType, submitData.getRate());
+                giveRateChr(chr, rateType, submitData.getRate());
+                break;
+            case 11:
+                giveGMChr(chr, submitData.getQuantity());
+                break;
+            case 12:
+                giveFameChr(chr, submitData.getQuantity());
                 break;
         }
     }
@@ -125,14 +137,9 @@ public class GiveService {
         log.info("管理员在后台给全服发放了 {} {}", quantity, getCashTypeName(type));
     }
 
-    private void giveNxChr(int wId, int cId, int quantity, int type) {
-        Character chr = Server.getInstance()
-                .getWorlds().get(wId)
-                .getPlayerStorage().getCharacterById(cId);
-
-        if (chr == null) throw new BizException("玩家已离线");
+    private void giveNxChr(Character chr, int quantity, int type) {
         chr.getCashShop().gainCash(type, quantity);
-        chr.message("管理员给你发放了 " + quantity + " " + getCashTypeName(type));
+        chr.message("管理员给您发放了 " + quantity + " " + getCashTypeName(type));
         log.info("管理员在后台给玩家 [{}] {} 发放了 {} {}", chr.getId(), chr.getName(), quantity, getCashTypeName(type));
     }
 
@@ -152,15 +159,9 @@ public class GiveService {
         log.info("管理员在后台给全服发放了 {} 金币", quantity);
     }
 
-    private void giveMesosChr(int wId, int cId, int quantity) {
-        Character chr = Server.getInstance()
-                .getWorlds().get(wId)
-                .getPlayerStorage().getCharacterById(cId);
-
-        if (chr == null) throw new BizException("玩家已离线");
-
+    private void giveMesosChr(Character chr, int quantity) {
         chr.gainMeso(quantity);
-        chr.message("管理员给你发放了 " + quantity + " 金币");
+        chr.message("管理员给您发放了 " + quantity + " 金币");
         log.info("管理员在后台给玩家 [{}] {} 发放了 {} 金币", chr.getId(), chr.getName(), quantity);
     }
 
@@ -172,15 +173,9 @@ public class GiveService {
         log.info("管理员在后台给全服发放了 {} 经验", quantity);
     }
 
-    private void giveExpChr(int wId, int cId, int quantity) {
-        Character chr = Server.getInstance()
-                .getWorlds().get(wId)
-                .getPlayerStorage().getCharacterById(cId);
-
-        if (chr == null) throw new BizException("玩家已离线");
-
+    private void giveExpChr(Character chr, int quantity) {
         chr.gainExp(quantity);
-        chr.message("管理员给你发放了 " + quantity + " 经验");
+        chr.message("管理员给您发放了 " + quantity + " 经验");
         log.info("管理员在后台给玩家 [{}] {} 发放了 {} 经验", chr.getId(), chr.getName(), quantity);
     }
 
@@ -223,7 +218,7 @@ public class GiveService {
 
     }
 
-    private void giveItemChr(int wId, int cId, int itemId, short quantity) {
+    private void giveItemChr(Character chr, int itemId, short quantity) {
         ItemInformationProvider ii = ItemInformationProvider.getInstance();
 
         String itemName = ii.getName(itemId);
@@ -241,18 +236,12 @@ public class GiveService {
             petId = Pet.createPet(itemId);
         }
 
-        Character chr = Server.getInstance()
-                .getWorlds().get(wId)
-                .getPlayerStorage().getCharacterById(cId);
-
-        if (chr == null) throw new BizException("玩家已离线");
-
         if (isPet) {
             InventoryManipulator.addById(chr.getClient(), itemId, quantity, "WAdmin", petId, expiration);
-            chr.message("管理员给你发放了 " + quantity + " 天 " + itemName);
+            chr.message("管理员给您发放了 " + quantity + " 天 " + itemName);
         } else {
             InventoryManipulator.addById(chr.getClient(), itemId, quantity, "WAdmin", -1, (short) 0, -1);
-            chr.message("管理员给你发放了 " + quantity + " 个 " + itemName);
+            chr.message("管理员给您发放了 " + quantity + " 个 " + itemName);
         }
 
         if (isPet) {
@@ -315,15 +304,13 @@ public class GiveService {
         );
     }
 
-    private void giveEquipChr(int wId, int cId, GiveResourceReqDTO submitData) {
+    private void giveEquipChr(Character chr, GiveResourceReqDTO submitData) {
         ItemInformationProvider ii = ItemInformationProvider.getInstance();
 
         String itemName = ii.getName(submitData.getId());
         if (ii.getEquipById(submitData.getId()) == null || itemName == null) {
             throw new BizException("装备不存在");
         }
-        Character chr = Server.getInstance().getWorld(wId).getPlayerStorage().getCharacterById(cId);
-        if (chr == null) throw new BizException("玩家已离线");
         chr.gainEquip(
                 submitData.getId(),
                 submitData.getStr(),
@@ -344,9 +331,9 @@ public class GiveService {
                 submitData.getUpgradeSlot(),
                 submitData.getExpire()
         );
-        chr.message("管理员给玩家 [" + cId + "] " + chr.getName() + " 发放了自定义装备 [" + submitData.getId().toString() + "] " + itemName);
+        chr.message("管理员给玩家 [" + chr.getId() + "] " + chr.getName() + " 发放了自定义装备 [" + submitData.getId().toString() + "] " + itemName);
         log.info("管理员在后台给玩家 [{}] {} 发放了自定义装备 [{}] {} 力量：{} 敏捷：{} 智力：{} 运气：{} HP：{} MP：{} 物攻：{} 魔攻：{} 物防：{} 魔防：{} 命中：{} 回避：{} 手技：{} 移速：{} 跳跃：{} 升级次数：{} 有效期：{} 分钟",
-                cId,
+                chr.getId(),
                 chr.getName(),
                 submitData.getId(),
                 itemName,
@@ -370,19 +357,36 @@ public class GiveService {
         );
     }
 
-    private void giveRateChr(int wId, int cId, String type, float rate) {
-        Character chr = Server.getInstance().getWorld(wId).getPlayerStorage().getCharacterById(cId);
-        if (chr == null) throw new BizException("玩家已离线");
-
+    private void giveRateChr(Character chr, String type, float rate) {
         ExtendValueDO data = ExtendValueDO.builder()
                 .extendId(String.valueOf(chr.getId()))
-                .extendType("21")
+                .extendType(ExtendType.CHARACTER_EXTEND.getType())
                 .extendName(type)
                 .extendValue(String.valueOf(rate))
                 .build();
         characterService.updateRate(data);
         
-        chr.message("管理员将你的 " + type + " 调整为：" + rate);
+        chr.message("管理员将您的 " + type + " 调整为：" + rate);
         log.info("管理员在后台将玩家 [{}] {} 的 {} 调整为：{}", chr.getId(), chr.getName(), type, rate);
+    }
+
+    private void giveGMChr(Character chr, Integer level) {
+        // 按照以下顺序hide，否则因为没有GM权限而无法hide或unhide
+        if (level < 3) {
+            chr.Hide(false);
+            chr.setGMLevel(level);
+        } else {
+            chr.setGMLevel(level);
+            chr.Hide(true);
+        }
+        chr.message("管理员将您的 GM等级 调整为：" + level);
+        log.info("管理员在后台将玩家 [{}] {} 的 GM等级 调整为：{}", chr.getId(), chr.getName(), level);
+    }
+    
+    private void giveFameChr(Character chr, Integer fame) {
+        chr.setFame(fame);
+        chr.updateSingleStat(Stat.FAME, fame);
+        chr.message("管理员将您的 人气 调整为：" + fame);
+        log.info("管理员在后台将玩家 [{}] {} 的 人气 调整为：{}", chr.getId(), chr.getName(), fame);
     }
 }
