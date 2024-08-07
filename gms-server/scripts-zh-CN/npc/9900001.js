33b9f8f188d00c9eb03f8d7d0322a2187c8fc694
昨日小睡 <1099365937@qq.com>
2024-08-07 14:43:16 +0800

    1.支持NextLevel与有状态脚本混用 2.支持示例脚本在拍卖行脚本中演示

--- gms-server/scripts-zh-CN/npc/9900001.js
+++ gms-server/scripts-zh-CN/npc/9900001.js
@@ -63,6 +63,7 @@ function action(mode, type, selection) {
             text += "\t\t\t\t#r=====以下内容仅GM可见=====\r\n";
             text += "#L61#超级传送#l \t #L62#超级商店#l \t #L63#整容集合#l\r\n\r\n";
 			text += "#L64#UI查询#l \t #L65#一键删除道具#l \t #L66#一键刷道具#l";
+			text += "#L67#有状态脚本示例#l \t #L68NextLevel脚本示例#l";
         }
         cm.sendSimple(text);
     } else if (status === 1) {
@@ -113,7 +114,13 @@ function doSelect(selection) {
             break;
         case 66:
             openNpc("一键刷道具");
-            break;				
+            break;
+        case 67:
+            openNpc("Example1")
+            break;
+        case 68:
+            openNpc("Example2")
+            break;
         default:
             cm.sendOk("该功能暂不支持，敬请期待！");
             cm.dispose();
