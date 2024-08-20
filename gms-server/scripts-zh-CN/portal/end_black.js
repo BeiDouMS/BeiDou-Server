6a296e4de7844d5d50b777d06ae2d92600d8515b
g9502995 <azxs9502995@gmail.com>
2024-08-21 00:03:34 +0800

    扰乱诺特勒斯号修復

---
+++ gms-server/scripts-zh-CN/portal/end_black.js
@@ -0,0 +1,11 @@
+/*
+	By: Kevin
+	Map: Hidden Chamber - The Nautilus - Stable (912000100)
+	Quest: Find Fresh Milk (2180)
+*/
+
+function enter(pi) {
+    pi.playPortalSound();
+    pi.warp(120000200, 0);
+    return true;
+}
