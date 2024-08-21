e433604c7e3e013253be758daca6241f24700b5b
g9502995 <azxs9502995@gmail.com>
2024-08-21 10:19:15 +0800

    Update 1092007.js

--- gms-server/scripts/npc/1092007.js
+++ gms-server/scripts/npc/1092007.js
@@ -1,13 +1,12 @@
 /*
 NPC:        Muirhat - Nautilus' Port
 Created By: Kevin
-Function:   When on the quest, he warps player to Black Magician's Disciple
+Function:   Ask the player if they want to be warped to Black Magician's Disciple
 */
 
 var status;
 
 function start() {
-
     status = -1;
     action(1, 0, 0);
 }
@@ -28,20 +27,13 @@ function action(mode, type, selection) {
         }
 
         if (status == 0) {
-            if (cm.getQuestStatus(2175) == 1) {
-                if (cm.getPlayer().canHold(2030019)) {
-                    cm.sendOk("Please take this #b#t2030019##k, it will make your life a lot easier.  #i2030019#");
-                } else {
-                    cm.sendOk("No free inventory spot available. Please make room in your USE inventory first.");
-                    cm.dispose();
-                }
-            } else {
-                cm.sendOk("The Black Magician and his followers. Kyrin and the Crew of Nautilus. \n They'll be chasing one another until one of them doesn't exist, that's for sure.");
-                cm.dispose();
-            }
+            cm.sendYesNo("Do you want to be warped to the Black Magician's Disciple?");
         } else if (status == 1) {
-            cm.gainItem(2030019, 1);
-            cm.warp(100000006, 0);
+            if (mode == 1) { // Player chose "Yes"
+                cm.warp(912000000, 0);
+            } else { // Player chose "No"
+                cm.sendOk("The Black Magician and his followers. Kyrin and the Nautilus crew. They will pursue each other until one of them is gone, that's for sure.");
+            }
             cm.dispose();
         }
     }
