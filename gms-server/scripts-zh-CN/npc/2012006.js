2ecc2c45c6034886c3540aa8f180d659288d63f4
昨日小睡 <1099365937@qq.com>
2024-08-29 20:00:35 +0800

    补充汉化消失的地方

--- gms-server/scripts-zh-CN/npc/2012006.js
+++ gms-server/scripts-zh-CN/npc/2012006.js
@@ -7,7 +7,7 @@ var boatType = ["飞船", "飞船", "飞艇", "鹤", "精灵", "渡船"];
 function start() {
     var message = "天空之城的站台有许多个月台，请按照你的目的地进行选择。你想要前往哪里？\r\n";
     for (var i = 0; i < destinations.length; i++) {
-        message += "\r\n#L" + i + "##b前往" + destinations[i] + "的月台.#l";
+        message += "\r\n#L" + i + "##b前往" + destinations[i] + "的" + boatType[i] + "站台.#l";
     }
     cm.sendSimple(message);
 }
