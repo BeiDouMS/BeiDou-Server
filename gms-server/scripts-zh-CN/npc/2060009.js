aec04a647c26e7cc486e59a94235307db82b9114
Jarecl <jarecl1996@gmail.com>
2024-08-29 14:10:23 +0800

    [游戏内优化] 1.海底世界出租车新增 海底叉路传送选项 2.中文js脚本中,未将选项换行,导致文本和选择挤在一行

--- gms-server/scripts-zh-CN/npc/2060009.js
+++ gms-server/scripts-zh-CN/npc/2060009.js
@@ -2,6 +2,7 @@ var status = 0;
 var menu;
 var payment = false;
 var atHerbTown = false;
+var locationCloseToOrbis =[13,18];
 
 function start() {
     if (cm.getPlayer().getMap().getId() == 251000100) {
@@ -12,17 +13,17 @@ function start() {
         if (atHerbTown) {
             menu = "#L0##b我想用 #t4031242##k 移动到 #b#m230030200##k.#l\r\n#L1#去 #b#m230000000##k 需支付 #b10000金币#k.#l";
         } else {
-            menu = "#L0##b我想用 #t4031242##k 移动到 #b#m230030200##k.#l\r\n#L1#去 #b#m251000000##k 需支付 #b10000金币#k.#l";
+            menu = "#L0##b我想用 #t4031242##k 移动到 #b#m230030200##k.#l\r\n#L1#去 #b#m251000000##k 需支付 #b10000金币#k.#l\r\n#L2#前往 #b#m230010000##k 需支付#b1000金币#k.#l";
         }
     } else {
         if (atHerbTown) {
             menu = "#L0#前往 #b#m230030200##k 需支付#b1000金币#k.#l\r\n#L1#前往 #b#m230000000##k 需支付#b10000金币#k.#l";
         } else {
-            menu = "#L0#前往 #b#m230030200##k 需支付#b1000金币#k.#l\r\n#L1#前往 #b#m251000000##k 需支付#b10000金币#k.#l";
+            menu = "#L0#前往 #b#m230030200##k 需支付#b1000金币#k.#l\r\n#L1#前往 #b#m251000000##k 需支付#b10000金币#k.#l\r\n#L2#前往 #b#m230010000##k 需支付#b1000金币#k.#l";
         }
         payment = true;
     }
-    cm.sendSimple("海洋之间都是相互连接的。无法步行到达的地方可以很容易地通过海路到达。今天和我们一起乘坐 #b海豚出租车#k，怎么样？"+menu);
+    cm.sendSimple("海洋之间都是相互连接的。无法步行到达的地方可以很容易地通过海路到达。今天和我们一起乘坐 #b海豚出租车#k，怎么样？\r\n"+menu);
 }
 
 function action(mode, type, selection) {
@@ -51,6 +52,17 @@ function action(mode, type, selection) {
             } else {
                 cm.gainMeso(-10000);
                 cm.warp(atHerbTown ? 230000000 : 251000100);
+            }
+        }
+        else if (selection == 2) {
+            if (cm.getPlayer().getMeso() < 1000) {
+                cm.sendOk("我认为你没有足够的钱……");
+                cm.dispose();
+                return;
+            } else {
+                cm.gainMeso(-1000);
+                //传送到通天塔入口最近的2个点随机一个其实就是在map.wz的div的13和18里面选
+                cm.warp(230010000,locationCloseToOrbis[Math.floor(Math.random() * locationCloseToOrbis.length)]);
             }
         }
         cm.dispose();
