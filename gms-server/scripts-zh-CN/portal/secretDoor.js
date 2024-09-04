a70fe2dc586d78cc6e96fa9d7cbd4ef24f732273
g9502995 <azxs9502995@gmail.com>
2024-09-04 21:17:49 +0800

    3360任务修复

--- gms-server/scripts-zh-CN/portal/secretDoor.js
+++ gms-server/scripts-zh-CN/portal/secretDoor.js
@@ -22,24 +22,19 @@
  */
 
 function doorCross(pi) {
-    pi.playPortalSound();
-    pi.warp(261030000, "sp_" + ((pi.getMapId() == 261010000) ? "jenu" : "alca"));
-    return true;
-}
+     pi.playPortalSound();
+     pi.warp(261030000, "sp_" + ((pi.getMapId() == 261010000) ? "jenu" : "alca"));
+     return true;
+ }
 
 function enter(pi) {
-    if (pi.isQuestCompleted(3360)) {
-        return doorCross(pi);
-    } else if (pi.isQuestStarted(3360)) {
-        var pw = pi.getQuestProgress(3360);
-        if (pw.length > 1) {
-            pi.openNpc(2111024, "MagatiaPassword");
-            return false;
-        } else {
-            return doorCross(pi);
-        }
-    } else {
-        pi.message("This door is locked.");
-        return false;
-    }
+     if (pi.isQuestCompleted(3360)) {
+         return doorCross(pi);
+     } else if (pi.isQuestStarted(3360)) {
+             pi.openNpc(2111024, "MagatiaPassword");
+             return false;
+         } else {
+         pi.message("门锁住了.");
+         return false;
+     }
 }
