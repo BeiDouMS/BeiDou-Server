58988a48f48f6c2eabd495e9d24c86dc56c1ef52
昨日小睡 <1099365937@qq.com>
2024-09-05 19:39:18 +0800

    修复报错

--- gms-server/src/main/java/org/gms/model/dto/InventoryequipmentRtnDTO.java
+++ gms-server/src/main/java/org/gms/model/dto/InventoryequipmentRtnDTO.java
@@ -2,8 +2,6 @@ package org.gms.model.dto;
 
 import com.mybatisflex.annotation.Column;
 import lombok.*;
-import org.gms.dao.entity.InventoryequipmentDO;
-
 
 @Data
 @AllArgsConstructor
@@ -57,33 +55,4 @@ public class InventoryequipmentRtnDTO {
     private Integer itemlevel;
 
     private Long itemexp;
-
-
-        public InventoryequipmentRtnDTO(InventoryequipmentDO inventoryequipmentDO) {
-
-            this.inventoryequipmentid = inventoryequipmentDO.getInventoryequipmentid();
-            this.inventoryitemid = inventoryequipmentDO.getInventoryitemid();
-            this.upgradeslots = inventoryequipmentDO.getUpgradeslots();
-            this.level = inventoryequipmentDO.getLevel();
-            this.str = inventoryequipmentDO.getStr();
-            this.dex = inventoryequipmentDO.getDex();
-            this.inte = inventoryequipmentDO.getInte();
-            this.luk = inventoryequipmentDO.getLuk();
-            this.hp = inventoryequipmentDO.getHp();
-            this.mp = inventoryequipmentDO.getMp();
-            this.watk = inventoryequipmentDO.getWatk();
-            this.matk = inventoryequipmentDO.getMatk();
-            this.wdef = inventoryequipmentDO.getWdef();
-            this.mdef = inventoryequipmentDO.getMdef();
-            this.acc = inventoryequipmentDO.getAcc();
-            this.avoid = inventoryequipmentDO.getAvoid();
-            this.hands = inventoryequipmentDO.getHands();
-            this.speed = inventoryequipmentDO.getSpeed();
-            this.jump = inventoryequipmentDO.getJump();
-            this.locked = inventoryequipmentDO.getLocked();
-            this.vicious = inventoryequipmentDO.getVicious();
-            this.itemlevel = inventoryequipmentDO.getItemlevel();
-            this.itemexp = inventoryequipmentDO.getItemexp();
-
-    }
 }
