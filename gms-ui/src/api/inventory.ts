8ad2da5039e5734e857d326c7a9dc98f4134d474
leevccc <94272011+leevccc@users.noreply.github.com>
2024-09-11 20:28:21 +0800

    新增：物品管理编辑和删除功能

--- gms-ui/src/api/inventory.ts
+++ gms-ui/src/api/inventory.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { InventoryState } from '@/store/modules/inventory/type';
 
-export interface inventoryCondition {
+export interface InventoryCondition {
   inventoryType?: number;
   characterId?: number;
   characterName?: string;
@@ -13,10 +14,18 @@ export function getInventoryTypeList() {
   return axios.get('/inventory/v1/getInventoryTypeList');
 }
 
-export function getCharacterList(condition: inventoryCondition) {
+export function getCharacterList(condition: InventoryCondition) {
   return axios.post('/inventory/v1/getCharacterList', condition);
 }
 
-export function getInventoryList(condition: inventoryCondition) {
+export function getInventoryList(condition: InventoryCondition) {
   return axios.post('/inventory/v1/getInventoryList', condition);
+}
+
+export function updateInventory(data: InventoryState) {
+  return axios.post('/inventory/v1/updateInventory', data);
+}
+
+export function deleteInventory(data: InventoryState) {
+  return axios.post('/inventory/v1/deleteInventory', data);
 }
--- gms-ui/src/views/game/inventory/table.vue
+++ gms-ui/src/views/game/inventory/table.vue
@@ -29,24 +29,76 @@
       <!--        align="center"-->
       <!--      />-->
       <a-table-column title="位置" data-index="position" align="center" />
-      <a-table-column title="数量" data-index="quantity" align="center" />
+      <a-table-column title="数量" align="center" :width="160">
+        <template #cell="{ record }">
+          <span v-if="editId !== record.id">
+            {{ record.quantity }}
+          </span>
+          <a-input-number v-else v-model="record.quantity" />
+        </template>
+      </a-table-column>
       <a-table-column title="签名" data-index="owner" align="center" />
       <a-table-column title="宠物ID" data-index="petId" align="center" />
       <a-table-column title="Flag" data-index="flag" align="center" />
       <a-table-column title="赠送人" data-index="giftFrom" align="center" />
       <a-table-column title="到期时间" align="center">
         <template #cell="{ record }">
-          {{ timestampToChineseTime(record.expiration) }}
+          <span v-if="editId !== record.id">
+            {{ timestampToChineseTime(record.expiration) }}
+          </span>
+          <a-input-number v-else v-model="record.expiration" />
         </template>
       </a-table-column>
       <!--      <a-table-column title="装备" data-index="equipment" align="center" />-->
+      <a-table-column title="操作" :width="200">
+        <template #cell="{ record }">
+          <a-button
+            v-if="editId !== record.id"
+            type="text"
+            size="mini"
+            @click="editId = record.id"
+          >
+            编辑
+          </a-button>
+          <a-button
+            v-if="editId === record.id"
+            type="text"
+            size="mini"
+            status="success"
+            @click="saveClick(record)"
+          >
+            保存
+          </a-button>
+          <a-button
+            v-if="editId === record.id"
+            type="text"
+            size="mini"
+            @click="editId = undefined"
+          >
+            取消
+          </a-button>
+          <a-popconfirm
+            v-if="editId !== record.id"
+            type="error"
+            content="你确定要删除这个道具吗？"
+            @ok="deleteClick(record)"
+          >
+            <a-button type="text" size="mini" status="danger"> 删除 </a-button>
+          </a-popconfirm>
+        </template>
+      </a-table-column>
     </template>
   </a-table>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue';
-  import { getInventoryList, inventoryCondition } from '@/api/inventory';
+  import {
+    deleteInventory,
+    getInventoryList,
+    InventoryCondition,
+    updateInventory,
+  } from '@/api/inventory';
   import useLoading from '@/hooks/loading';
   import { InventoryState } from '@/store/modules/inventory/type';
   import { getIconUrl } from '@/utils/mapleStoryAPI';
@@ -58,15 +110,17 @@
     currentType: string | number;
     characterId: number | undefined;
   }>();
+  const editId = ref<number | undefined>(undefined);
 
   const loadData = async () => {
+    editId.value = undefined;
     // 可在选择完玩家后刷新
     if (!props || !props.characterId) {
       return;
     }
     setLoading(true);
     try {
-      const condition: inventoryCondition = {
+      const condition: InventoryCondition = {
         inventoryType: props.currentType as number,
         characterId: props.characterId as number,
         pageNo: 1,
@@ -95,6 +149,26 @@
 
     // 格式化输出为中文时间字符串
     return `${year}年${month}月${day}日 ${hours}时${minutes}分${seconds}秒`;
+  };
+
+  const saveClick = async (data: InventoryState) => {
+    setLoading(true);
+    try {
+      await updateInventory(data);
+      await loadData();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteClick = async (data: InventoryState) => {
+    setLoading(true);
+    try {
+      await deleteInventory(data);
+      await loadData();
+    } finally {
+      setLoading(false);
+    }
   };
 </script>
 
