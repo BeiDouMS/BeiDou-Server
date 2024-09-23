eb7d76fdd65f4a04edb47a0a565ee3b033e2968e
幻幻 <magical_email@qq.com>
2024-09-24 01:01:41 +0800

    修复：所有组队任务支持解除最低人数限制，不支持解除人数上限 汉化：部分脚本进行汉化

--- gms-server/scripts-zh-CN/event/PiratePQ.js
+++ gms-server/scripts-zh-CN/event/PiratePQ.js
@@ -40,6 +40,9 @@ var eventTime = 4;     // 4 minutes
 
 const maxLobbies = 1;
 
+const YamlConfig = Java.type('org.gms.config.YamlConfig');
+minPlayers = YamlConfig.config.server.USE_ENABLE_SOLO_EXPEDITIONS ? 1 : minPlayers;  //如果解除远征队人数限制，则最低人数改为1人
+
 function init() {
     setEventRequirements();
 }
