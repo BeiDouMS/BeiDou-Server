212f45a428d4bb7a20512ebb24df75e4ffbcb8bc
cbwsnr <cbwsnr@163.com>
2024-09-30 15:32:29 +0800

    gm命令从service层提取

---
+++ gms-server/src/main/java/org/gms/dao/entity/CommandInfoDO.java
@@ -0,0 +1,37 @@
+package org.gms.dao.entity;
+
+import com.mybatisflex.annotation.Id;
+import com.mybatisflex.annotation.KeyType;
+import com.mybatisflex.annotation.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("command_info")
+public class CommandInfoDO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id(keyType = KeyType.Auto)
+    private Integer id;
+
+    private Integer level;
+
+    private String syntax;
+
+    private String commandClassName;
+
+    private Boolean enabled;
+
+    private String description;
+
+}
