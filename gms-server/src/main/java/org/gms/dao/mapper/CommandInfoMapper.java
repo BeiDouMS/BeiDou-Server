789038fc242e92cee8adf74324f13be551bf8bd7
cbwsnr <cbwsnr@163.com>
2024-10-01 19:38:23 +0800

    数据库脚本初始化指令表，指令加载从数据库读取

---
+++ gms-server/src/main/java/org/gms/dao/mapper/CommandInfoMapper.java
@@ -0,0 +1,14 @@
+package org.gms.dao.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import org.gms.dao.entity.CommandInfoDO;
+
+/**
+ * gm指令
+ *
+ * @author cbwsnr
+ * @since 2024-10-01
+ */
+public interface CommandInfoMapper extends BaseMapper<CommandInfoDO> {
+
+}
