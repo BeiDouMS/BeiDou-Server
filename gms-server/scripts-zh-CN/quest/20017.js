1962f32ece08688b64bc3bbf81703d6440e60445
Xiaojie Zhou <hiugiak@outlook.com>
2024-10-03 13:24:06 +0800

    fix: 骑士团任务20017异常 & 对话文案与Say.img对齐

--- gms-server/scripts-zh-CN/quest/20017.js
+++ gms-server/scripts-zh-CN/quest/20017.js
@@ -1,6 +1,6 @@
 /*
-	NPC Name: 		Cygnus
-	Description: 		Quest - Encounter with the Young Queen
+    NPC Name: 		Cygnus
+    Description: 		Quest - Encounter with the Young Queen
 */
 
 var status = -1;
@@ -14,21 +14,20 @@ function start(mode, type, selection) {
     if (mode == 1) {
         status++;
     } else {
-	if (status == 2) {
-	    qm.sendNext("嗯，没什么好担心的。对你这样的人来说，这将是轻而易举的事。鼓起勇气，准备好了就告诉我.");
-	    qm.dispose();
-	    return;
-	}
+        if (status == 2) {
+            qm.dispose();
+            return;
+        }
         status--;
     }
     if (status == 0) {
-    	qm.sendNext("喔? #p1101002# 派你来的, 呵呵？你一定是最近加入天鹅座骑士团的新手。欢迎，很高兴见到你！我是 #p1102000#. 我是培训老师，培训所有像你这样的贵族。当然，我不是你能说的那种人.");
+        qm.sendNext("嗯？是#p1101002#派你过来的吗？啊哈！你是这次新来的吗？很高兴认识你。很高兴认识你！我的名字是#p1102000#。是专门教你这种贵族的骑士训练师。嗯...你怎么这么看着我...啊，你好像是第一次见到菲约吧。");
     } else if (status == 1) {
-    	qm.sendNextPrev("我们叫皮约斯。你已经看到了 #p1101001# 她一直在皇后身边，不是吗? 小精灵和 #p1101001#, 但我们属于不同的类型. 当然，你没见过我们，因为我们只住在埃雷夫。你很快就会习惯吃比萨饼的.");
+        qm.sendNextPrev("我们种族被称为菲约。你和女王旁边的#p1101001#说过话吗？菲约是和#p1101001#一样的种族。虽然派系有些不同...差不多。我们只生活在圣地，虽然会有些陌生，但很快就会熟悉的。");
     } else if (status == 2) {
-    	qm.sendNextPrev("哦，你知不知道埃雷夫里面没有怪物？一点邪恶都不敢进入地狱。但别担心。你将能够训练由叫做咪咪的幻觉怪物.");
+        qm.sendNextPrev("啊，你或许知道吧？在这圣地根本不存在怪物。拥有邪恶力量的存在是无法涉足圣地的。不过不要担心。训练是以#p1101001#所创造的幻想生物绢毛鸟作为对象来进行的。那么开始训练吗？");
     } else if (status == 3) {
-    	qm.sendAcceptDecline("你好像准备好了！看看你所取得的成就，我认为你应该马上开始寻找更高级的咪咪。. 现在需要你去猎杀 #b15个 #r#o100122#s 在 #m130010100##k#k? 通过左边的路口可以到达 #b 训练林2 #k.");
+        qm.sendAcceptDecline("运好气了吧！那么...看你的实力，你应该可以捕猎比中等稍强一点的绢毛鸟。捕猎在#m130010100#的15只#o0100122#该足够了吧？怎么样？能搞定#o0100122#吗？");
     } else if (status == 4) {
         qm.guideHint(12);
         qm.forceStartQuest(20020);
@@ -36,7 +35,4 @@ function start(mode, type, selection) {
         qm.forceStartQuest();
         qm.dispose();
     }
-}
-
-function end(mode, type, selection) {
 }
--- gms-server/scripts/quest/20017.js
+++ gms-server/scripts/quest/20017.js
@@ -1,6 +1,6 @@
 /*
-	NPC Name: 		Cygnus
-	Description: 		Quest - Encounter with the Young Queen
+    NPC Name: 		Cygnus
+    Description: 		Quest - Encounter with the Young Queen
 */
 
 var status = -1;
@@ -15,20 +15,19 @@ function start(mode, type, selection) {
         status++;
     } else {
         if (status == 2) {
-            qm.sendNext("Hmm, there is nothing to worry about. This will be a breeze for someone your level. Muster your courage and let me know when you're ready.");
             qm.dispose();
             return;
         }
         status--;
     }
     if (status == 0) {
-        qm.sendNext("Hmm? #p1101002# sent you, huh? You must be the newbie that recently joined Cygnus Knights. Welcome, and nice to meet you! My name is #p1102000#. I'm the Training Instructor who trains all Noblesses like yourself. Of course, I'm not a human as you can tell.");
+        qm.sendNext("What&apos;s that? #p1101002# sent you? Oh! Wow, are you a newbie? Good to meet ya. Really. I&apos;m #p1102000#. I&apos;m the Training Instructor who&apos;s in charge of Noblesse like you. But... why are you looking at me like that...? Ah, this must be your first time seeing a Piyo.");
     } else if (status == 1) {
-        qm.sendNextPrev("We are called Piyos. You've seen #p1101001# who is at the Empress's side all the time, haven't you? Piyos are of the same family as #p1101001#, but we belong to different types. Of course, you haven't seen any of us since we only live in Ereve. You'll get used to Piyos in no time.");
+        qm.sendNextPrev("We are called Piyos. You&apos;ve talked to #p1101001# who&apos;s always next to the Empress, haven&apos;t you? Well, Piyos are from the same family as #p1101001# though we differ in type. Piyos only live in Ereve, so I&apos;m sure it&apos;ll take some time for you to get used to us.");
     } else if (status == 2) {
-        qm.sendNextPrev("Oh, and did you know that there are no monsters in Ereve? Not even a smidgeon of evil dare enter Ereve. But don't you worry. You'll be able to train with illusory monsters created by #p1101001# called Mimis.");
+        qm.sendNextPrev("Oh, and did you know that there are no monsters in Ereve? Not even a smidgeon of evil dare enter Ereve. But don&apos;t you worry. You&apos;ll be able to train with illusory monsters created by #p1101001# called Mimis. Shall we begin the training now?");
     } else if (status == 3) {
-        qm.sendAcceptDecline("You seem prepared! Looking at what you've accomplished, I think you should jump right into hunting more advanced Mimis. How about you hunt #b15 #r#o100122#s in #m130010100##k#k? Use the portal on the left to reach the #bTraining Forest II#k.");
+        qm.sendAcceptDecline("shall we begin your second training then? Let&apos;s have you hunt #r#o100123##ks this time. It should be a bit more challenging. What are #o100123#s, you wonder? You probably saw them while you were hunting the #o100122#s... They somewhat resemble the #o10012");
     } else if (status == 4) {
         qm.guideHint(12);
         qm.forceStartQuest(20020);
@@ -36,7 +35,4 @@ function start(mode, type, selection) {
         qm.forceStartQuest();
         qm.dispose();
     }
-}
-
-function end(mode, type, selection) {
 }
