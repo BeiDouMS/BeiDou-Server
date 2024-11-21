d9e3a22d7a01d6ea61bf951fbd20db5712cc39d8
昨日小睡 <1099365937@qq.com>
2024-11-21 17:26:15 +0800

    自动创建数据库支持环境变量配置

--- gms-server/src/main/java/org/gms/ServerApplication.java
+++ gms-server/src/main/java/org/gms/ServerApplication.java
@@ -2,6 +2,7 @@ package org.gms;
 
 import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.gms.util.RequireUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -37,8 +38,8 @@ public class ServerApplication {
      */
     private static void initDb(String[] args) throws Exception {
         InputStream resource = null;
-        // 解析 jvm option -Dspring.config.location
-        String location = System.getProperty("spring.config.location");
+
+        String location = getStartParam(args, "spring.config.location");
         if (location != null) {
             Path path = Path.of(location);
             if (!Files.exists(path)) {
@@ -47,31 +48,22 @@ public class ServerApplication {
             resource = Files.newInputStream(path);
         }
         if (resource == null) {
-            // 解析 spring argument --spring.config.location
-            for (String arg : args) {
-                if (arg.startsWith("--spring.config.location=")) {
-                    location = arg.split("=")[1];
-                    Path path = Path.of(location);
-                    if (!Files.exists(path)) {
-                        return;
-                    }
-                    resource = Files.newInputStream(path);
-                    break;
-                }
-            }
-        }
-        if (resource == null) {
             // 解析jar包自带的yml
             resource = ServerApplication.class.getClassLoader().getResourceAsStream("application.yml");
         }
+
         Yaml yaml = new Yaml();
         LinkedHashMap<String, Object> property = yaml.load(resource);
         JSONObject mybatisFlex = JSONObject.parse(JSONObject.toJSONString(property.get("mybatis-flex")));
         JSONObject datasource = mybatisFlex.getJSONObject("datasource").getJSONObject("mysql");
-        String driver = datasource.getString("driver-class-name");
-        String dbUrl = datasource.getString("url");
-        String username = datasource.getString("username");
-        String password = datasource.getString("password");
+        String driver = getStartParam(args, "mybatis-flex.datasource.mysql.driver-class-name");
+        if (driver == null) driver = datasource.getString("driver-class-name");
+        String dbUrl = getStartParam(args, "mybatis-flex.datasource.mysql.url");
+        if (dbUrl == null) dbUrl = datasource.getString("url");
+        String username = getStartParam(args, "mybatis-flex.datasource.mysql.username");
+        if (username == null) username = datasource.getString("username");
+        String password = getStartParam(args, "mybatis-flex.datasource.mysql.password");
+        if (password == null) password = datasource.getString("password");
         String urlPrefix = dbUrl.split("\\?")[0];
         String[] dbSplit = urlPrefix.split("/");
         String dbName = dbSplit[dbSplit.length - 1];
@@ -92,5 +84,21 @@ public class ServerApplication {
     private static Connection getConnection(String driver, String url, String username, String password) throws Exception {
         Class.forName(driver);
         return DriverManager.getConnection(url, username, password);
+    }
+
+    private static String getStartParam(String[] args, String paramName) {
+        // 第一优先级 jvm参数
+        String property = System.getProperty(paramName);
+        if (property != null) {
+            return property;
+        }
+        // 第二优先级 springboot参数
+        for (String arg : args) {
+            if (arg.startsWith("--" + paramName)) {
+                return arg.split("=")[1];
+            }
+        }
+        // 第三优先级 环境变量
+        return System.getenv(paramName.replaceAll("\\.", "_"));
     }
 }
