922a598ee4b8ae2fa7661aabc4b2babf05c906a6
幻幻 <magical_email@qq.com>
2024-11-22 09:57:41 +0800

    优化：修正部分代码。

--- gms-server/src/main/java/org/gms/server/SystemRescue.java
+++ gms-server/src/main/java/org/gms/server/SystemRescue.java
@@ -20,34 +20,31 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+@Getter
 public class SystemRescue {
     private static final Logger log = LoggerFactory.getLogger(Client.class);
-    @Getter
     private static final List<Integer> MapIdList = Arrays.asList(100000000, 101000000, 102000000, 103000000, 104000000);   //射手村，魔法密林，勇士部落，废弃都市，明珠港
     private static final String Lebel = I18nUtil.getLogMessage("SystemRescue.info.map.label") + " ";
-    private static Boolean isChangeChanneling = false;
+    private static boolean isChangeChanneling = false;
 
     /**
      * 卡地图救援，将某个倒霉蛋解救到其它地图。
-     * @param player
-     * @param cause
+     * @param player 传入玩家角色
      */
-    public void setMapChange(Character player,Throwable cause){
+    public void setMapChange(Character player){
         try {
-            log.info("判断是否卡地图");
-            if (player == null || cause == null) return;   //预防空指针
+            if (player == null) return;   //预防空指针
 
             int MapId = GameConfig.getServerInt("system_rescue_maperror_changeid");    //该参数为控制台配置，设置地图ID大于0则启用。设置不存在的ID则改为随机传送到金银岛某个城镇。
             if (MapId > 0) {
                 // 判断条件是否满足，角色登录时isChangeChanneling = true，满足则判定问异常掉线
                 if (isChangeChanneling){
-                    Boolean Map_exists = false;
+                    boolean Map_exists = false;
                     int MapId_error = player.getMapId();       //读取出错地图ID
                     String MapName_error = player.getMap().getMapName().isEmpty() ? I18nUtil.getLogMessage("SystemRescue.info.map.message1") : player.getMap().getMapName();  //读取出错地图名称，这里是读取服务端String.wz地图名称，不存在则设为 未知地图
                     try {
-                        Map_exists = MapId == MapId_error ? false : !player.getWarpMap(MapId).getMapName().isEmpty();  //如果异常掉线地图和设置的救援地图一致 或者 地图名称为空  或者  地图文件wz不存在则会报错
-                    } catch (Exception e) {
-                        Map_exists = false;
+                        Map_exists = MapId != MapId_error && !player.getWarpMap(MapId).getMapName().isEmpty();  //如果异常掉线地图和设置的救援地图一致 或者 地图名称为空  或者  地图文件wz不存在则会报错
+                    } catch (Exception ignored) {
                     }
                     // 如果地图不存在，随机选择一个新的地图ID
                     if (!Map_exists) {
@@ -78,9 +75,10 @@ public class SystemRescue {
     public void setChangeChannelState(Boolean state){
         isChangeChanneling = state;
     }
+
     /**
      * 卡地图救援：玩家重新上线后会收到提示信息。
-     * @param player
+     * @param player 传入玩家角色
      */
     public void showMapChange(Character player) {
         try {
@@ -88,7 +86,7 @@ public class SystemRescue {
             //聊天窗口红色提示
             if (DropMessage(player, "系统救援_卡地图_系统通知", 5)) {
                 String Message = GameConfig.getServerString("system_rescue_maperror_message");     //该参数为控制台配置
-                if (Message != null && Message.length() > 0) {
+                if (Message != null && !Message.isEmpty()) {
                     //卡地图救援提示判断，弹窗
                     player.dropMessage(1, Message);
                 }
@@ -99,15 +97,15 @@ public class SystemRescue {
     }
 
     /**
-     * 处理发送给玩家的消息。
+     * 处理玩家的消息掉落。
      *
-     * @param {Character} player - 玩家对象。
-     * @param {String} keyname - 存储消息的键名。
-     * @param {int} type - 消息类型。1=消息弹窗，5=红色聊天消息，6=蓝色聊天消息
-     * @returns {Boolean} - 如果成功处理消息返回 true，否则返回 false。
+     * @param player 玩家对象。
+     * @param keyname 存储消息的键名。
+     * @param type 消息类型。
+     * @return 如果成功处理消息返回 true，否则返回 false。
      */
     private Boolean DropMessage(Character player,String keyname,int type) {
-        if(player == null || keyname == null || keyname == "") return false;  //预防空指针
+        if(player == null || keyname == null || keyname.isEmpty()) return false;  //预防空指针
         String Message = player.getAbstractPlayerInteraction().getCharacterExtendValue(keyname, true);      //从玩家扩展表里提取指定键值
         if(Message != null && Message.length() > 1) {
             player.dropMessage(type,Message);       //给指定玩家发送消息
