df3de959e61e0d462c67f862ae3dd6b0da26ae5c
546285798@qq.com <546285798@qq.com>
2024-12-14 22:44:04 +0800

    修复蘑菇城任务，越过城墙(2)无法进入任务地图的bug

--- gms-server/scripts-zh-CN/portal/gotocastle.js
+++ gms-server/scripts-zh-CN/portal/gotocastle.js
@@ -1,10 +1,18 @@
 function enter(pi) {
-    if (pi.isQuestCompleted(2324)) {
-        pi.playPortalSound();
-        pi.warp(106020501, 0);
-        return true;
-    } else {
+    if(pi.isQuestCompleted(2321)){
+        if (pi.isQuestCompleted(2324)) {
+            pi.playPortalSound();
+            pi.warp(106020501, 0);
+            return true;
+        }
+        else{
+            pi.playPortalSound();
+            pi.warp(106020500, 0);
+            return true;
+        }
+    }
+    else {
         pi.playerMessage(5, "前路布满荆棘，无法通过！");
         return false;
     }
-}
+ }
