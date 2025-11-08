4793d596cca62ff82d9a0daa4b8b4e9ceed2207c
ziqiming <zqm@qq.com>
2025-11-08 22:50:25 +0800

    修复女神塔通关后召唤雅典娜女神无奖励的问题
    1、修复后台报错
    2、修复增加经验奖励
    3、显示客户端过关特效

--- gms-server/scripts-zh-CN/reactor/2006001.js
+++ gms-server/scripts-zh-CN/reactor/2006001.js
@@ -25,12 +25,11 @@
  */
 
 function act() {
+	var eim = rm.getEventInstance();
     rm.spawnNpc(2013002);
-    rm.getEventInstance().clearPQ();
-
-    rm.getEventInstance().setProperty("statusStg8", "1");
+    eim.setProperty("statusStg8", "1");
     eim.giveEventPlayersExp(3500);
     eim.showClearEffect(true);
-
-    rm.getEventInstance().startEventTimer(5 * 60000); //bonus time
+    eim.startEventTimer(5 * 60000); //bonus time
+	eim.clearPQ();
 }
