5f75d29d3835a23ef77f124e4e2de609ebb03aa4
ziqiming <zqm@qq.com>
2025-11-10 21:31:42 +0800

    修复之前在上一个传送点全员带队员没有奖励的问题
    发送碎片应该是这个传送点！

--- gms-server/scripts-zh-CN/portal/party6_out.js
+++ gms-server/scripts-zh-CN/portal/party6_out.js
@@ -2,16 +2,18 @@ function enter(pi) {
     var eim = pi.getEventInstance();
 
     if (eim.isEventCleared()) {
-        if (pi.isEventLeader()) {
+        if (pi.canHold(4001198, 1)) {
+            pi.gainItem(4001198, 1);
             pi.playPortalSound();
-            eim.warpEventTeam(930000800);
+            pi.warp(930000800);
+            pi.playerMessage(5, "将石头人的毒株投入净化之泉");
             return true;
-        } else {
-            pi.playerMessage(5, "请等待队长先通过传送门");
-            return false;
+        }else{
+            pi.playerMessage(5, "背包需空出1格来接收奖励。");
         }
+       
     } else {
-        pi.playerMessage(5, "请先消灭剧毒魔像");
+        pi.playerMessage(5, "请先消灭石头人");
         return false;
     }
 }
