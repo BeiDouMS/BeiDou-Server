170e5e7f82fd1e5dbec80d21eb3e663a819e85d6
WH_WalkerW <xz_it_walkerw@esuperfund.com.au>
2025-11-17 16:57:10 +0800

    修复：脚本内数据类型不正确。

--- gms-server/scripts-zh-CN/portal/davy2_hd1.js
+++ gms-server/scripts-zh-CN/portal/davy2_hd1.js
@@ -2,7 +2,7 @@ var windowTime = 10 * 1000;
 
 function enter(pi) {
     var eim = pi.getEventInstance();
-    var level = eim.getProperty("level");
+    var level = eim.getIntProperty("level");
     if (eim.getProperty("stage2b") == "0") {
         pi.getMap(925100202).spawnAllMonstersFromMapSpawnList(level, true);
         eim.setProperty("stage2b", "1");
