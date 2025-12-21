0c900ce3daf5d914d9fe1bd25c9470b43b6e6297
ziqiming <zqm@qq.com>
2025-12-21 10:01:37 +0800

    修复点击创建和解散家族询问点击否后导致的卡NPC问题
    解决：询问后点否，再点NPC就点不开了！

--- gms-server/scripts-zh-CN/npc/2010007.js
+++ gms-server/scripts-zh-CN/npc/2010007.js
@@ -1,46 +1,28 @@
-/*
-	This file is part of the OdinMS Maple Story Server
-    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
-		       Matthias Butz <matze@odinms.de>
-		       Jan Christian Meyer <vimes@odinms.de>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation version 3 as published by
-    the Free Software Foundation. You may not use, modify or distribute
-    this program under any other version of the GNU Affero General Public
-    License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-/* guild creation npc */
-var status = 0;
+var status;
 var sel;
 
 function start() {
-    cm.sendSimple("欢迎来到家族公馆，你现在想做什么呢?\r\n#b#L0#创建家族#l\r\n#L1#解散家族#l\r\n#L2#增加家族成员人数上限#l#k");
+    status = -1;
+    action(1, 0, 0);
 }
 
 function action(mode, type, selection) {
-    if (mode == -1) {
+    if (mode < 0) {
         cm.dispose();
     } else {
-        if (mode == 0 && status == 0) {
-            cm.dispose();
-            return;
-        }
-        if (mode == 1) {
+       if (mode == 1) {
             status++;
         } else {
             status--;
         }
-        if (status == 1) {
+
+        if(status == 0){
+
+            cm.sendSimple("欢迎来到家族公馆，你现在想做什么呢?\r\n#b#L0#创建家族#l\r\n#L1#解散家族#l\r\n#L2#增加家族成员人数上限#l#k");
+
+        } else if (status == 1) {
+
             sel = selection;
             if (selection == 0) {
                 if (cm.getPlayer().getGuildId() > 0) {
@@ -78,6 +60,8 @@ function action(mode, type, selection) {
                     cm.dispose();
                 }
             }
+        }else{
+             cm.dispose();
         }
     }
 }
