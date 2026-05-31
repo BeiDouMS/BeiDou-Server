27529d68d5906eb24e5bbd5ea9cb8086ae25ca22
昨日小睡 <1099365937@qq.com>
2026-05-31 17:02:54 +0800

    更改打包设置

--- gms-server/launch.bat
+++ gms-server/launch.bat
@@ -2,5 +2,5 @@
 @title BeiDou
 chcp 65001
 
-.\jdk-21.0.2\bin\java.exe  -Dspring.config.location=application.yml -jar BeiDou.jar
+.\jdk-21.0.11+10-jre\bin\java.exe  -Dspring.config.location=application.yml -jar BeiDou.jar
 pause
--- gms-server/launch.sh
+++ gms-server/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # cover write
-./jdk-21.0.2/bin/java -Dspring.config.location=application.yml -jar BeiDou.jar &
+./jdk-21.0.11+10-jre/bin/java -Dspring.config.location=application.yml -jar BeiDou.jar &
