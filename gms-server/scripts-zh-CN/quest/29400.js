2f7c061d746ff00a29f7b41b77a1aa743a49f542
skykiseki <evelinesdd@qq.com>
2026-06-03 23:10:10 +0800

    fix勤奋冒险家勋章任务
    勤奋冒险家勋章，修复接了之后没反应的bug

---
+++ gms-server/scripts-zh-CN/quest/29400.js
@@ -0,0 +1,56 @@
+/*
+	Author: BubblesDev
+	Description: Quest - Veteran Hunter (29400) - 勤奋冒险家勋章
+	Quest ID: 29400
+*/
+
+var status = -1;
+
+function start(mode, type, selection) {
+    if (mode == -1) {
+        qm.dispose();
+    } else {
+        if (mode == 1) {
+            status++;
+        } else {
+            qm.dispose();
+            return;
+        }
+
+        if (status == 0) {
+            qm.sendNext("在30天内狩猎高于自己等级的#r100000只#k怪物！完成任务即可获得#e#b勤奋冒险家勋章#k#n称号。其中实力最为出众的勇士将获得#e#b传说中的猎人(特级)#k#n……");
+        } else if (status == 1) {
+            qm.sendYesNo("请注意：必须为高于自身等级的怪物（120级以上的角色必须狩猎120级以上的怪物）。你要接受这个挑战吗？");
+        } else if (status == 2) {
+            qm.forceStartQuest();
+            qm.showInfoText("你已接受勤奋冒险家勋章挑战！");
+            qm.dispose();
+        }
+    }
+}
+
+function end(mode, type, selection) {
+    if (mode == -1) {
+        qm.dispose();
+    } else {
+        if (mode != 1 && type > 0) {
+            qm.dispose();
+            return;
+        }
+
+        status++;
+        if (status == 0) {
+            qm.sendNext("恭喜你获得#b<勤奋冒险家>勋章#k！\r\n\r\n#fUI/UIWindow.img/QuestIcon/4/0#\r\n #v1142004:# #t1142004# 1");
+        } else if (status == 1) {
+            if (qm.canHold(1142004)) {
+                qm.gainItem(1142004);
+                qm.forceCompleteQuest();
+                qm.dispose();
+            } else {
+                qm.sendNext("请在勋章栏位中腾出空间。");
+            }
+        } else if (status == 2) {
+            qm.dispose();
+        }
+    }
+}
---
+++ gms-server/scripts/quest/29400.js
@@ -0,0 +1,56 @@
+/*
+	Author: BubblesDev
+	Description: Quest - Veteran Hunter (29400)
+	Quest ID: 29400
+*/
+
+var status = -1;
+
+function start(mode, type, selection) {
+    if (mode == -1) {
+        qm.dispose();
+    } else {
+        if (mode == 1) {
+            status++;
+        } else {
+            qm.dispose();
+            return;
+        }
+
+        if (status == 0) {
+            qm.sendNext("Hunt #r100000#k monsters that are higher level than you within 30 days! Anyone who completes this task will receive the title, #e#bVeteran Hunter#k#n. The person with the highest score will receive the #e#bLegendary Hunter (Special Title)#k#n.");
+        } else if (status == 1) {
+            qm.sendYesNo("Note that monsters must be higher level than your character. Characters level 120 or above must hunt monsters level 120 or higher. Do you want to accept this challenge?");
+        } else if (status == 2) {
+            qm.forceStartQuest();
+            qm.showInfoText("You have accepted the Veteran Hunter medal challenge!");
+            qm.dispose();
+        }
+    }
+}
+
+function end(mode, type, selection) {
+    if (mode == -1) {
+        qm.dispose();
+    } else {
+        if (mode != 1 && type > 0) {
+            qm.dispose();
+            return;
+        }
+
+        status++;
+        if (status == 0) {
+            qm.sendNext("Congratulations on earning your honorable #b<Veteran Hunter>#k title. Keep up the good work!\r\n\r\n#fUI/UIWindow.img/QuestIcon/4/0#\r\n #v1142004:# #t1142004# 1");
+        } else if (status == 1) {
+            if (qm.canHold(1142004)) {
+                qm.gainItem(1142004);
+                qm.forceCompleteQuest();
+                qm.dispose();
+            } else {
+                qm.sendNext("Please make room in your medal/necklace inventory.");
+            }
+        } else if (status == 2) {
+            qm.dispose();
+        }
+    }
+}
