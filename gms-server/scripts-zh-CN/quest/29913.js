68d6d63c845b14cea723efab7c161044715440d5
skykiseki <skykiseki@users.noreply.github.com>
2026-06-25 00:21:49 +0800

    fix: implement ultimate job medal quests

---
+++ gms-server/scripts-zh-CN/quest/29913.js
@@ -0,0 +1,56 @@
+/*
+	This file is part of the OdinMS Maple Story Server
+    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
+		       Matthias Butz <matze@odinms.de>
+		       Jan Christian Meyer <vimes@odinms.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation version 3 as published by
+    the Free Software Foundation. You may not use, modify or distribute
+    this program under any other version of the GNU Affero General Public
+    License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+	Author : 		kevintjuh93
+	Description: 		Quest - Legendary Thief
+	Quest ID : 		29913
+*/
+
+var status = -1;
+
+function start(mode, type, selection) {
+    if (qm.forceStartQuest()) {
+        qm.showInfoText("您已经获得了<影飞侠>头衔。您可以从达赖尔获得勋章。");
+    }
+    qm.dispose();
+}
+
+function end(mode, type, selection) {
+    status++;
+    if (mode != 1) {
+        qm.dispose();
+    } else {
+        if (status == 0) {
+            qm.sendNext("恭喜你获得了 #b<影飞侠>#k 头衔。祝您在未来的冒险中一切顺利！继续加油吧，勇士。\r\n\r\n#fUI/UIWindow.img/QuestIcon/4/0#\r\n #v1142012:# #t1142012# 1");
+        } else if (status == 1) {
+            if (qm.canHold(1142012)) {
+                qm.gainItem(1142012);
+                qm.forceCompleteQuest();
+                qm.dispose();
+            } else {
+                qm.sendNext("请给背包留出空位");//NOT GMS LIKE
+            }
+        } else if (status == 2) {
+            qm.dispose();
+        }
+    }
+}
