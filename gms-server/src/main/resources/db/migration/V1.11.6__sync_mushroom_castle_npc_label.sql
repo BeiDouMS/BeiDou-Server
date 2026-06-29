329b155419e8c395016be23ebd2b7fe0ad167d01
skykiseki <skykiseki@users.noreply.github.com>
2026-06-29 23:58:06 +0800

    Simplify Mushroom Castle NPC migration setup

--- gms-server/src/main/resources/db/migration/V1.11.6__sync_mushroom_castle_npc_label.sql
+++
@@ -1,8 +0,0 @@
-UPDATE game_config
-SET config_value = CASE
-    WHEN config_value REGEXP '1300005:"[^"]*"' THEN REGEXP_REPLACE(config_value, '1300005:"[^"]*"', '1300005:"蘑菇王国"')
-    WHEN TRIM(config_value) = '{}' THEN '{9001105:"Rescue Gaga!",1300005:"蘑菇王国"}'
-    WHEN RIGHT(TRIM(config_value), 1) = '}' THEN CONCAT(LEFT(TRIM(config_value), LENGTH(TRIM(config_value)) - 1), ',1300005:"蘑菇王国"}')
-    ELSE '{9001105:"Rescue Gaga!",1300005:"蘑菇王国"}'
-END
-WHERE config_code = 'npcs_scriptable';
--- gms-server/src/main/resources/db/migration/V1.8.8__update_mushroom_castle_npc_label.sql
+++
@@ -1,4 +0,0 @@
-UPDATE game_config
-SET config_value = REPLACE(config_value, '1300005:"The Test"', '1300005:"蘑菇王国"')
-WHERE config_code = 'npcs_scriptable'
-  AND config_value LIKE '%1300005:"The Test"%';
